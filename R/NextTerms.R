8d75702d66dfacd03d686528dd8f01ac2acba36e
Sbo <sbo@trolling.dk>
2019-12-03 21:19:38 +0100

    Completed Quiz 3 (semi nonsense it seems)

--- R/NextTerms.R
+++ R/NextTerms.R
@@ -21,6 +21,8 @@ NextTerms <- function(inputText = "") {
 #  print(inputText)
 
   matchFound <- FALSE
+  result <- NA
+#  matchesInHigherOrderGrams <- 0 # Keep track of matches in higher order n-grams, don't count hit again for lower order matches
   
     
   if (partialWord)
@@ -29,7 +31,8 @@ NextTerms <- function(inputText = "") {
 #    cat("Partial word\n")
     # Search for string, start with quadgrams, then trigrams, then bigrams lastly unigrams
     i <- 3
-    while (!matchFound & i >= 0) {
+#    while (!matchFound & i >= 0) {
+    while (i >= 0) {
       matchstring <- str_match(inputText, paste("([:alpha:]+ ){", i ,"}[:alpha:]+$", sep = ""))[1]
       if (!is.na(matchstring)) {
 #        print(matchstring)
@@ -38,8 +41,15 @@ NextTerms <- function(inputText = "") {
 #        print(dim(matches))
         if (dim(matches)[1] > 1 | (dim(matches)[1] == 1 & nchar(matchstring) < nchar(matches[1]))) matchFound <- TRUE
         # Remove potential identical match
-        if (dim(matches)[1] > 1) matches <- matches[which(nchar(matches$term) > nchar(matchstring)),]
+#        if (dim(matches)[1] > 1) matches <- matches[which(nchar(matches$term) > nchar(matchstring)),]
+        if (dim(matches)[1] > 0) matches <- matches[which(nchar(matches$term) > nchar(matchstring)),]
 #        print(matches)
+        if (dim(matches)[1] > 0){
+          matchFound <- TRUE
+          matches$score <- matches$count / dim(matches)[1]
+          matches$dictionary <- dictionary[i+1]
+          result <- rbind(result, matches)
+        }
       }
       i <- i -1
     }
@@ -52,15 +62,25 @@ NextTerms <- function(inputText = "") {
       # Search for string, start with quadgrams, then trigrams and lastly bigrams
       i <- 3
 #      matchFound <- FALSE
-      while (!matchFound & i >= 1) {
+      while (i >= 1) {
+#      while (!matchFound & i >= 1) {
         matchstring <- str_match(inputText, paste("([:alpha:]+ ){", i-1 ,"}[:alpha:]+$", sep = ""))[1]
         if (!is.na(matchstring)) {
-          print(matchstring)
-          cat(paste("Searching in ", dictionary[i+1], "\n", sep = ""))
+#          print(matchstring)
+#          cat(paste("Searching in ", dictionary[i+1], "\n", sep = ""))
           matches <- eval(as.name(dictionary[i+1]))[which(startsWith(eval(as.name(dictionary[i+1]))$term, paste(matchstring, " ", sep = ""))),]
 #          print(dim(matches))
-          if (dim(matches)[1] > 0) matchFound <- TRUE
-#          print(matches)
+          if (dim(matches)[1] > 0){
+            matchFound <- TRUE
+            matches$score <- matches$count / dim(matches)[1]
+            matches$dictionary <- dictionary[i+1]
+            result <- rbind(result, matches)
+          }
+
+          # test
+          # matches <- matches %>%
+          #   arrange(desc(score))
+          # print(matches)
         }
         i <- i -1
       }
@@ -69,10 +89,13 @@ NextTerms <- function(inputText = "") {
   
   if (matchFound)
   {
-    result <- matches %>%
-      arrange(desc(count)) %>%
-      mutate(term = str_match(term, "[:alpha:]+$"))
+    result <- result %>%
+      mutate(term = str_match(term, "[:alpha:]+$")) %>%
+      group_by(term) %>%                  # Summarize scores from all n-gram dictionaries
+      summarise(score = sum(score)) %>%
+      arrange(desc(score))
     
+      
   } else
   {
     result <- NA
--- R/Quiz3.R
+++ R/Quiz3.R
@@ -14,9 +14,50 @@ load(paste(outputDir ,"05-CreateTermFrequencies.RData", sep = ""))
 
 
 
-res <- NextTerms("When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd ") %>%
+# Not give
+# Correct: die
+NextTerms("When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd ") %>%
   filter(term %in% c("give", "sleep", "die", "eat"))
 
-res <- NextTerms("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his ") %>%
+
+# Not financial
+# Not spiritual
+# Correct: maritial
+NextTerms("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his ") %>%
   filter(term %in% c("marital", "spiritual", "financial", "horticultural"))
+
+# Not morning
+# Correct: weekend
+NextTerms("I'd give anything to see arctic monkeys this ") %>%
+  filter(term %in% c("weekend", "month", "morning", "decade"))
+
+# Correct: stress
+NextTerms("Talking to your mom has the same effect as a hug and helps reduce your ") %>%
+  filter(term %in% c("happiness", "sleepiness", "hunger", "stress"))
+
+# Not look
+# Correct: picture
+NextTerms("When you were in Holland you were like 1 inch away from me but you hadn't time to take a ") %>%
+  filter(term %in% c("look", "walk", "minute", "picture"))
+
+# Not case
+# Correct: matter
+NextTerms("I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the ") %>%
+  filter(term %in% c("incident", "case", "account", "matter"))
+
+# Correct: hand
+NextTerms("I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each ") %>%
+  filter(term %in% c("hand", "arm", "finger", "toe"))
+
+# Correct: top
+NextTerms("Every inch of you is perfect from the bottom to the ") %>%
+  filter(term %in% c("top", "center", "middle", "side"))
+
+# Correct: outside
+NextTerms("I’m thankful my childhood was filled with imagination and bruises from playing ") %>%
+  filter(term %in% c("weekly", "daily", "outside", "inside"))
+
+# Correct: movies
+NextTerms("I like how the same people are in almost all of Adam Sandler ") %>%
+  filter(term %in% c("novels", "stories", "movies", "pictures"))
 
